25d1d8c13bd58d53fe0c1ef74655812e9ff9014e
Anna Kuznetsova <aakuznetsova@gmail.com>
2017-06-09 09:08:21 -0700

    add business logic to js, function that returns 'ping' if number is divisible by 3,'pong' if number is divisible by 5 and 'ping-pong' if number is divisible by 15

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,18 @@ $(document).ready(function() {
 
 // BACK END
 function ping_pong(number) {
-  return "test";
+  for (i = 1; i <= number; i++) {
+    if (number % 15 === 0) {
+      number = "ping_pong";
+      return number;
+    } else if (number % 5 === 0) {
+      number = "pong";
+      return number;
+    } else if (number % 3 === 0) {
+      number = "ping";
+      return number;
+    }
+
+  }
+
 }
