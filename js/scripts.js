af7e703c08ad5d98393caa99df924384ddaaf9e5
Anna Kuznetsova <aakuznetsova@gmail.com>
2017-06-09 16:57:42 -0700

    add changes to js and html  files

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,33 @@
 //FRONT END
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
-    event.preventDefault();
     var number = parseInt($("#inputNumber").val());
     $("#result").text(ping_pong(number));
-
-    $("#reset").click(function(event){
-      $("#result").hide();
-      return;
-
+    event.preventDefault();
+    $("#reset").click(function() {
+      $("ul").empty();
     });
-
   });
 });
-
-
 
 // BACK END
 var arrayResult = [];
 function ping_pong(number) {
-  var arrayResult = Array.from({length: number}, (v, k) => k+1);
+  var arrayResult = Array.from({
+    length: number
+  }, (v, k) => k + 1);
   arrayResult.forEach(function(number) {
     if (number % 15 === 0) {
-      arrayResult.splice((number-1), 1, "ping-pong");
+      arrayResult.splice((number - 1), 1, "ping-pong");
     } else if (number % 5 === 0) {
-      arrayResult.splice((number-1), 1, "pong");
+      arrayResult.splice((number - 1), 1, "pong");
     } else if (number % 3 === 0) {
-      arrayResult.splice((number-1), 1, "ping");
+      arrayResult.splice((number - 1), 1, "ping");
     } else {
       return arrayResult
     }
   });
   arrayResult.forEach(function(number) {
-  $("ul").append("<li>" + number + "</li>");
+    $("ul").append("<li>" + number + "</li>");
   });
 }
