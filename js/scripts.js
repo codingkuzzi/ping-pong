6af6f63ea8d970f2e739e66a42f901aaf43c0760
Anna Kuznetsova <aakuznetsova@gmail.com>
2017-06-10 15:29:16 -0700

    add image and style form-group in css file

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,33 @@
 //FRONT END
 $(document).ready(function() {
-  REinit();
-});
-
-function REinit() {
   $("form#formOne").submit(function(event) {
     var number = parseInt($("#inputNumber").val());
     $("#result").text(ping_pong(number));
     event.preventDefault();
-
+    $("#reset").click(function() {
+      $("ul").empty();
+    });
   });
-  $("#reset").click(function(event){
-    $("#result").empty();
-    REinit();
-  });
-
-}
-
-
+});
 
 // BACK END
 var arrayResult = [];
 function ping_pong(number) {
-  var arrayResult = Array.from({length: number}, (v, k) => k+1);
+  var arrayResult = Array.from({
+    length: number
+  }, (v, k) => k + 1);
   arrayResult.forEach(function(number) {
     if (number % 15 === 0) {
-      arrayResult.splice((number-1), 1, "ping-pong");
+      arrayResult.splice((number - 1), 1, "ping-pong");
     } else if (number % 5 === 0) {
-      arrayResult.splice((number-1), 1, "pong");
+      arrayResult.splice((number - 1), 1, "pong");
     } else if (number % 3 === 0) {
-      arrayResult.splice((number-1), 1, "ping");
+      arrayResult.splice((number - 1), 1, "ping");
     } else {
       return arrayResult
     }
   });
   arrayResult.forEach(function(number) {
-
     $("ul").append("<li>" + number + "</li>");
   });
 }
